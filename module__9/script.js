311f761277e75055c673e380d7f749e7c1a02777
Touhid <140738779+Kyotoka@users.noreply.github.com>
2025-05-30 17:53:20 +0600

    Update script.js

--- module__9/script.js
+++ module__9/script.js
@@ -1,4 +1,4 @@
-let tableBody = document.querySelector("#post-table tbody");
+/*let tableBody = document.querySelector("#post-table tbody");
 
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response)=>response.json())
@@ -13,26 +13,34 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     tableBody.appendChild(row);
   });
 })
+*/
+let row = document.querySelector(".row");
 
-// let tableBody = document.querySelector("#postsTable tbody");
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
 
-// const fetchData = async () => {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-//     const posts = await response.json();
+    posts.slice(0, 12).forEach((post) => {
+      // let row = document.createElement("tr");
 
-//     posts.slice(0, 10).forEach((post) => {
-//       let row = document.createElement("tr");
-//       row.innerHTML = `
-//         <td>${post.id}</td>
-//         <td>${post.title}</td>
-//         <td>${post.body}</td>
-//       `;
-//       tableBody.appendChild(row);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+      row.innerHTML += `
+        <div class="col-4">
+          <div class="card bg-dark text-white mb-3 border p-3">
+            <h2>${post.id}</h2>
+            <p>${post.title}</p>
+            <p>${post.body}</p>
+          </div>
+        </div>
+      `;
+
+      // row.appendChild(row);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
 
 // fetchData();
